b65867d4c7d70d8678f468fbe9eab5039953c817
tunetun123 <fathurrahmansyuaib@gmail.com>
2024-09-24 10:29:23 +0800

    khanza update 19 september 2024

--- src/fungsi/akses.java
+++ src/fungsi/akses.java
@@ -230,7 +230,9 @@ public final class akses {
             balance_cairan=false,catatan_observasi_chbp=false,catatan_observasi_induksi_persalinan=false,skp_kategori_penilaian=false,skp_kriteria_penilaian=false,
             skp_penilaian=false,referensi_poli_mobilejknfktp=false,referensi_dokter_mobilejknfktp=false,skp_rekapitulasi_penilaian=false,pembayaran_pihak_ke3_bankmandiri=false,
             metode_pembayaran_bankmandiri=false,bank_tujuan_transfer_bankmandiri=false,kodetransaksi_tujuan_transfer_bankmandiri=false,konsultasi_medik=false,jawaban_konsultasi_medik=false,
-            pcare_cek_alergi=false,pcare_cek_prognosa=false;
+            pcare_cek_alergi=false,pcare_cek_prognosa=false,data_sasaran_usiaproduktif=false,data_sasaran_usialansia=false,skrining_perilaku_merokok_sekolah_remaja=false,
+            skrining_kekerasan_pada_perempuan=false,skrining_obesitas=false,skrining_risiko_kanker_payudara=false,skrining_risiko_kanker_paru=false,skrining_tbc=false,
+            skrining_kesehatan_gigi_mulut_remaja=false,penilaian_awal_keperawatan_ranap_bayi=false,booking_mcu_perusahaan=false;
     
     public static void setData(String user, String pass) {
         try {        
@@ -1302,6 +1304,17 @@ public final class akses {
                         akses.jawaban_konsultasi_medik=true;
                         akses.pcare_cek_alergi=true;
                         akses.pcare_cek_prognosa=true;
+                        akses.data_sasaran_usiaproduktif=true;
+                        akses.data_sasaran_usialansia=true;
+                        akses.skrining_perilaku_merokok_sekolah_remaja=true;
+                        akses.skrining_kekerasan_pada_perempuan=true;
+                        akses.skrining_obesitas=true;
+                        akses.skrining_risiko_kanker_payudara=true;
+                        akses.skrining_risiko_kanker_paru=true;
+                        akses.skrining_tbc=true;
+                        akses.skrining_kesehatan_gigi_mulut_remaja=true;
+                        akses.penilaian_awal_keperawatan_ranap_bayi=true;
+                        akses.booking_mcu_perusahaan=true;
                     }else if(rs2.getRow()>=1){   
                         rs2.beforeFirst();
                         rs2.next();
@@ -2357,6 +2370,17 @@ public final class akses {
                         akses.jawaban_konsultasi_medik=rs2.getBoolean("jawaban_konsultasi_medik");
                         akses.pcare_cek_alergi=rs2.getBoolean("pcare_cek_alergi");
                         akses.pcare_cek_prognosa=rs2.getBoolean("pcare_cek_prognosa");
+                        akses.data_sasaran_usiaproduktif=rs2.getBoolean("data_sasaran_usiaproduktif");
+                        akses.data_sasaran_usialansia=rs2.getBoolean("data_sasaran_usialansia");
+                        akses.skrining_perilaku_merokok_sekolah_remaja=rs2.getBoolean("skrining_perilaku_merokok_sekolah_remaja");
+                        akses.skrining_kekerasan_pada_perempuan=rs2.getBoolean("skrining_kekerasan_pada_perempuan");
+                        akses.skrining_obesitas=rs2.getBoolean("skrining_obesitas");
+                        akses.skrining_risiko_kanker_payudara=rs2.getBoolean("skrining_risiko_kanker_payudara");
+                        akses.skrining_risiko_kanker_paru=rs2.getBoolean("skrining_risiko_kanker_paru");
+                        akses.skrining_tbc=rs2.getBoolean("skrining_tbc");
+                        akses.skrining_kesehatan_gigi_mulut_remaja=rs2.getBoolean("skrining_kesehatan_gigi_mulut_remaja");
+                        akses.penilaian_awal_keperawatan_ranap_bayi=rs2.getBoolean("penilaian_awal_keperawatan_ranap_bayi");
+                        akses.booking_mcu_perusahaan=rs2.getBoolean("booking_mcu_perusahaan");
                     }else if((rs.getRow()==0)&&(rs2.getRow()==0)){
                         akses.kode="";                  
                         akses.penyakit= false;
@@ -3410,6 +3434,17 @@ public final class akses {
                         akses.jawaban_konsultasi_medik=false;
                         akses.pcare_cek_alergi=false;
                         akses.pcare_cek_prognosa=false;
+                        akses.data_sasaran_usiaproduktif=false;
+                        akses.data_sasaran_usialansia=false;
+                        akses.skrining_perilaku_merokok_sekolah_remaja=false;
+                        akses.skrining_kekerasan_pada_perempuan=false;
+                        akses.skrining_obesitas=false;
+                        akses.skrining_risiko_kanker_payudara=false;
+                        akses.skrining_risiko_kanker_paru=false;
+                        akses.skrining_tbc=false;
+                        akses.skrining_kesehatan_gigi_mulut_remaja=false;
+                        akses.penilaian_awal_keperawatan_ranap_bayi=false;
+                        akses.booking_mcu_perusahaan=false;
                     }
                 } catch (Exception e) {
                     System.out.println("Notifikasi : "+e);
@@ -4486,6 +4521,17 @@ public final class akses {
         akses.jawaban_konsultasi_medik=false;
         akses.pcare_cek_alergi=false;
         akses.pcare_cek_prognosa=false;
+        akses.data_sasaran_usiaproduktif=false;
+        akses.data_sasaran_usialansia=false;
+        akses.skrining_perilaku_merokok_sekolah_remaja=false;
+        akses.skrining_kekerasan_pada_perempuan=false;
+        akses.skrining_obesitas=false;
+        akses.skrining_risiko_kanker_payudara=false;
+        akses.skrining_risiko_kanker_paru=false;
+        akses.skrining_tbc=false;
+        akses.skrining_kesehatan_gigi_mulut_remaja=false;
+        akses.penilaian_awal_keperawatan_ranap_bayi=false;
+        akses.booking_mcu_perusahaan=false;
     }
     
     public static int getjml1() {return akses.jml1;}    
@@ -5578,4 +5624,15 @@ public final class akses {
     public static boolean getjawaban_konsultasi_medik(){return akses.jawaban_konsultasi_medik;}
     public static boolean getpcare_cek_alergi(){return akses.pcare_cek_alergi;}
     public static boolean getpcare_cek_prognosa(){return akses.pcare_cek_prognosa;}
+    public static boolean getdata_sasaran_usiaproduktif(){return akses.data_sasaran_usiaproduktif;}
+    public static boolean getdata_sasaran_usialansia(){return akses.data_sasaran_usialansia;}
+    public static boolean getskrining_perilaku_merokok_sekolah_remaja(){return akses.skrining_perilaku_merokok_sekolah_remaja;}
+    public static boolean getskrining_kekerasan_pada_perempuan(){return akses.skrining_kekerasan_pada_perempuan;}
+    public static boolean getskrining_obesitas(){return akses.skrining_obesitas;}
+    public static boolean getskrining_risiko_kanker_payudara(){return akses.skrining_risiko_kanker_payudara;}
+    public static boolean getskrining_risiko_kanker_paru(){return akses.skrining_risiko_kanker_paru;}
+    public static boolean getskrining_tbc(){return akses.skrining_tbc;}
+    public static boolean getskrining_kesehatan_gigi_mulut_remaja(){return akses.skrining_kesehatan_gigi_mulut_remaja;}
+    public static boolean getpenilaian_awal_keperawatan_ranap_bayi(){return akses.penilaian_awal_keperawatan_ranap_bayi;}
+    public static boolean getbooking_mcu_perusahaan(){return akses.booking_mcu_perusahaan;}
 }   
